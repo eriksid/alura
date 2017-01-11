a9066bb6a09894c1ea67336b33ffb4540c9055f2
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-01-11 10:55:36 -0200

    Adicionado tabela

--- php_e_mysql/produto-lista.php
+++ php_e_mysql/produto-lista.php
@@ -1,14 +1,22 @@
 <?php include("cabecalho.php");?>
 <?php include("conecta.php");?>
-<?php include("banco-produto.php");
+<?php include("banco-produto.php");?>
+
+<table class="table table-striped table-bordered">
+	<?php 
 
 
-$produtos = listaProdutos($conexao);
+	$produtos = listaProdutos($conexao);
+	foreach ($produtos as $produto) :
+		?>
+		<tr>
+	 		<td><?=$produto['nome']?></td>
+	 		<td><?=$produto['preco']?></td>
+	 	</tr>
+	 <?php 
+		endforeach
 
-foreach ($produtos as $produto) {
-	echo $produto['nome'] . "<br/>";
-}
+	?>
+</table>
 
-
-?>
 <?php include("rodape.php"); ?>
