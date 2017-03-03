e8546bc17e87dd26c3ec3a088704008f921b56fe
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-03-03 20:58:12 -0300

    .

---
+++ c# 1/CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroDacomta;
+            numeroDacomta = 1;
+            
+            double saldo = 100.0;
+            double valor = 10.0;
+            double saldoAposSaque = 100.0 - valor;
+
+            MessageBox.Show("O saldo atual da conta é: " + saldoAposSaque);
+
+        }
+    }
+}
--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,31 @@
-titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhvalido = true;
+
+if (peso <= 0 || peso >=1000){
+
+	pesoEhValido = false;
+	tdImc.textContent = "Peso invalido!";
+
+}
+
+if (altura <=0 || altura >=3.0) {
+	alturaEhvalido = false;
+	tdImc.textContent = "Altura invalida!";
+}
+if (pesoEhValido && alturaEhvalido){
+	imc = peso / (altura * altura);
+	
+
+	tdImc.textContent = imc;
+
+}
