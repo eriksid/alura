5e3abcc8c42a34400e9adbe5028bb07e96a89f4a
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-03-18 15:35:55 -0300

    Revert "."
    This reverts commit 7c957e32428045962c694bb5fe55138ec293a90b.

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,45 +1,31 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
 
-var pacientes = document.querySelectorAll(".paciente");
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(".info-imc");
 
-for (var i = 0 ; i <pacientes.length; i++) {
-	var paciente = pacientes[i];
-	console.log("paciente "+i+ paciente);
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
-	var tdImc = paciente.querySelector(".info-imc");
+var pesoEhValido = true;
+var alturaEhvalido = true;
 
-	var pesoEhValido = true;
-	var alturaEhvalido = true;
+if (peso <= 0 || peso >=1000){
 
-	if (peso <= 0 || peso >=1000){
+	pesoEhValido = false;
+	tdImc.textContent = "Peso invalido!";
 
-		pesoEhValido = false;
-		tdImc.textContent = "Peso invalido!";
-		paciente.classList.add("paciente-invalido");
-
-	}
-
-	if (altura <=0 || altura >=3.0) {
-		alturaEhvalido = false;
-		tdImc.textContent = "Altura invalida!";
-		paciente.classList.add("paciente-invalido");
-	}
-	if (pesoEhValido && alturaEhvalido){
-		imc = peso / (altura * altura);
-		
-
-		tdImc.textContent = imc.toFixed(2);
-
-	}
 }
 
-
+if (altura <=0 || altura >=3.0) {
+	alturaEhvalido = false;
+	tdImc.textContent = "Altura invalida!";
+}
+if (pesoEhValido && alturaEhvalido){
+	imc = peso / (altura * altura);
 	
 
+	tdImc.textContent = imc;
 
-
-
+}
